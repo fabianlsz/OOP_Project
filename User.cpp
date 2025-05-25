a4a512ebc6daa03c4579af27168dae5fc5d822bb
Jeworrek Oliver <hsvdgm0@gmail.com>
2025-05-25 14:09:45 +0300

    sortat

--- User.cpp
+++
@@ -1,7 +0,0 @@
-#include "User.h"
-
-User::User(const std::string& name,
-           const std::string& vorname,
-           const std::string& email,
-           const std::string& rolle)
-    : name(name), vorname(vorname), email(email), rolle(rolle) {}
